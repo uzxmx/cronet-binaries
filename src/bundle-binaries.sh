a6a515e04254a3f8c98853a5bb1528291790912d
Mingxiang Xue <mingxiangxue@gmail.com>
2025-01-11 15:45:15 +0800

    Update
    Signed-off-by: Mingxiang Xue <mingxiangxue@gmail.com>

--- src/bundle-binaries.sh
+++ src/bundle-binaries.sh
@@ -20,25 +20,27 @@ esac
 
 set -ex
 
-mkdir -p out/Release/cronet
+out="$1"
+
+mkdir -p $out/cronet
 
 if [ "$target_os" = 'android' ]; then
   WITH_SYSROOT='third_party/android_toolchain/ndk/toolchains/llvm/prebuilt/linux-x86_64/sysroot'
 fi
 
-cp -a out/Release/$shared_lib_name out/Release/cronet/
+cp -a $out/$shared_lib_name $out/cronet/
 if [ "$host_os" = 'win' ]; then
-  cp -a out/Release/$dll_name out/Release/cronet/
+  cp -a $out/$dll_name $out/cronet/
 fi
-cp -a out/Release/obj/components/cronet/$static_lib_name out/Release/cronet/
-cp -a components/cronet/native/generated/cronet.idl_c.h out/Release/cronet/
-cp -a components/cronet/native/include/cronet_c.h out/Release/cronet/
-cp -a components/cronet/native/include/cronet_export.h out/Release/cronet/
-cp -a components/grpc_support/include/bidirectional_stream_c.h out/Release/cronet/
+cp -a $out/obj/components/cronet/$static_lib_name $out/cronet/
+cp -a components/cronet/native/generated/cronet.idl_c.h $out/cronet/
+cp -a components/cronet/native/include/cronet_c.h $out/cronet/
+cp -a components/cronet/native/include/cronet_export.h $out/cronet/
+cp -a components/grpc_support/include/bidirectional_stream_c.h $out/cronet/
 if [ "$WITH_SYSROOT" ]; then
-  cp -a "$PWD/$WITH_SYSROOT" out/Release/cronet/sysroot
+  cp -a "$PWD/$WITH_SYSROOT" $out/cronet/sysroot
 fi
 if [ "$target_os" = 'android' ]; then
   # Included by base/BUILD.gn
-  cp -a base/android/library_loader/anchor_functions.lds out/Release/cronet
+  cp -a base/android/library_loader/anchor_functions.lds $out/cronet
 fi
